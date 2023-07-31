bb916503c0b927d256cb85dac3bca9aa66bb1e4b
Divine Eboigbe <ogbedivine7@gmail.com>
2023-07-31 11:35:14 +0100

    test for employee and person

---
+++ src/Inheritance/test1.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class test1 {
+    public static void main(String[] args) {
+//        Person person = new Person();
+        Employee employee = new Employee();
+
+//        person.setName("Divine");
+//        person.setAge(20);
+//        person.setGender("male");
+    }
+}
